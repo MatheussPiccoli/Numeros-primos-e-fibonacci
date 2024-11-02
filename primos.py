b3437d87aa45cb3981182d0f09d6b794430a394e
Matheus Piccoli de Moura <matheuspdmoura@gmail.com>
2024-11-02 02:31:22 -0300

    Update primos.py

--- primos.py
+++ primos.py
@@ -1,11 +1,7 @@
 #  2 - Números primos
-#  
 #      -- Criar uma função em sua linguagem preferida. A função deve receber um numero N > 1 (validar o input),
 # e retornar todos os números primos até o número N. EX. p(2) = [2]; p(3) = [2, 3]; p(10) = [2, 3, 5, 7];
-# 
-#     --- Criar uma função recursiva que resolva p
-# 
-#       --- Criar uma função linear que resolva p
+
 def primos(n):
     lista_primos = []
     for i in range(1, n+1):
